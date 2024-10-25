7e2464569a988afc747e9e984ae0814928d6abc9
Chloe <144107013+Charen523@users.noreply.github.com>
2024-10-25 14:58:06 +0900

    [Bronze I] Title: Africa, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/15629. Africa/Africa.cc
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+enum class Nations {
+	botswana = 0,
+	ethiopia,
+	kenya,
+	namibia,
+	south_africa,
+	tanzania,
+	zambia,
+	zimbabwe
+};
+
+unordered_map<string, Nations> nationsMap = {
+	{"botswana", Nations::botswana},
+	{"ethiopia", Nations::ethiopia},
+	{"kenya", Nations::kenya},
+	{"namibia", Nations::namibia},
+	{"south-africa", Nations::south_africa},
+	{"tanzania", Nations::tanzania},
+	{"zambia", Nations::zambia},
+	{"zimbabwe", Nations::zimbabwe}
+};
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int testCase;
+	cin >> testCase;
+
+	vector<Nations> visitOrder(testCase);
+	for (int i = 0; i < testCase; i++) {
+		string word;
+		cin >> word;
+		visitOrder[i] = nationsMap[word];
+	}
+
+	bool isSouthAfrica = false; //나미비아 비자 할인
+	bool isZ = false;
+	int result = 0;
+
+	for (int i = 0; i < testCase; i++) {
+		switch (visitOrder[i]) {
+		case Nations::botswana:
+			//무비자
+			break;
+		case Nations::ethiopia:
+			result += 50;
+			break;
+		case Nations::kenya:
+			result += 50;
+			break;
+		case Nations::namibia:
+			if (isSouthAfrica) result += 40;
+			else result += 140;
+			break;
+		case Nations::south_africa:
+			//무비자
+			isSouthAfrica = true;
+			break;
+		case Nations::tanzania:
+			result += 50;
+			break;
+		case Nations::zambia:
+			if (!isZ) result += 50;
+			if (i < testCase - 1 && visitOrder[i + 1] == Nations::zimbabwe) isZ = true;
+			break;
+		case Nations::zimbabwe:
+			if (!isZ) {
+				if (i < testCase - 1 && visitOrder[i + 1] == Nations::zambia) {
+					result += 50;
+					isZ = true;
+				}
+				else result += 30;
+			}
+			break;
+		}
+	}
+
+	cout << result;
+}
