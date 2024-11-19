916a4d3f01981e2af5b9a58e8fbdecef1bfdd450
Chloe <144107013+Charen523@users.noreply.github.com>
2024-11-19 12:16:44 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+vector<int> combination;
+void solve(int start) {
+	if (combination.size() == M) {
+		for (int c : combination) {
+			cout << c << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = start; i <= N; i++) {
+		combination.push_back(i);
+		solve(i + 1);
+		combination.pop_back();
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> N >> M;
+	solve(1); 
+}
