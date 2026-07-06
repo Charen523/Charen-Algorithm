56723a9d99505cb3669b970b56bc60012e25fdfe
Chloe <144107013+Charen523@users.noreply.github.com>
2026-07-06 13:21:29 +0900

    [level 2] Title: 튜플, Time: 26.30 ms, Memory: 6.52 MB -BaekjoonHub

--- C++/프로그래머스/2/64065. 튜플/튜플.cpp
+++ C++/프로그래머스/2/64065. 튜플/튜플.cpp
@@ -1,49 +1,56 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <unordered_set>
 
 using namespace std;
 
 vector<int> solution(string s) {
-    vector<vector<int>> sets;
     vector<int> answer;
-    vector<bool> used(100001, false);
-
-    vector<int> current;
-    string num = "";
-
-    for (int i = 1; i < s.size() - 1; i++) {
+    
+    vector<vector<int>> tuples;
+    
+    int row_ptr = 0;
+    string temp_int = "";
+    
+    s = s.substr(1, s.size() - 2);
+    int s_size = s.size();
+    for (int i = 0; i < s_size; i++) {
         char c = s[i];
-
-        if (c >= '0' && c <= '9') {
-            num += c;
-        }
-        else {
-            if (!num.empty()) {
-                current.push_back(stoi(num));
-                num = "";
-            }
-
-            if (c == '}') {
-                sets.push_back(current);
-                current.clear();
-            }
-        }
-    }
-
-    sort(sets.begin(), sets.end(), [](const vector<int>& a, const vector<int>& b) {
-        return a.size() < b.size();
-    });
-
-    for (const vector<int>& set : sets) {
-        for (int n : set) {
-            if (!used[n]) {
-                used[n] = true;
-                answer.push_back(n);
+        switch (c) {
+            case '\{':
+                tuples.push_back({});
                 break;
-            }
+            case ',':
+                tuples[row_ptr].push_back(stoi(temp_int));
+                temp_int = "";
+                break;
+            case '\}':
+                tuples[row_ptr].push_back(stoi(temp_int));
+                temp_int = "";
+                row_ptr++;
+                i++; //vector간 간격 무시
+                break;
+            default:
+                temp_int += c;
+                break;
         }
     }
-
+    
+    sort (tuples.begin(), tuples.end(), [](const vector<int> a, const vector<int> b){
+         return a.size() < b.size();
+    });
+    
+    unordered_set<int> result_hash;
+    int t_size = tuples.size();
+    for (int i = 0; i < t_size; i++){
+        for (int j = 0; j < i + 1; j++) {
+            int k = tuples[i][j];
+            if (result_hash.find(k) != result_hash.end()) continue;
+            answer.push_back(k);
+            result_hash.insert(k);
+        }
+    }
+    
     return answer;
 }
